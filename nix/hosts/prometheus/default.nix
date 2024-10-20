54a02a4d003bb8e956edab3cdde23297503a9212
Arian van Putten <arian.vanputten@gmail.com>
2024-10-20 19:02:39 +0200

    fix build

--- nix/hosts/prometheus/default.nix
+++ nix/hosts/prometheus/default.nix
@@ -1,7 +1,7 @@
 {
   services.prometheus = {
     enable = true;
-    globalConfigs.scrape_interval = "15s";
+    globalConfig.scrape_interval = "15s";
     scrapeConfigs = [{
       job_name = "node_exporter";
       ec2_sd_configs = [{
